84940bed894691f054619927b3c35720b6113ca4
Laura Adam <laurawynosky@gmail.com>
2018-05-24 20:40:55 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,10 +2,12 @@
 def introduction (name)
   puts"Hi, my name is #{name}."
 end
-introduction (Sophie)
+introduction ("Sophie")
+
 
 #introduction_with_language 
 def greeting_programmer (name, language)
-  puts"Hello, #{name}.  We heard you are a great #{langguage} programmer."
+  puts"Hello, #{name}. We heard you are a great #{language} programmer."
 end
-greeting_programmer ("Sophie", "Ruby")
+greeting_programmer ("Sophie","Ruby")
+
